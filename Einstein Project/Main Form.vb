87b0759b5095edb28ba22113a23d99efc3b0ff0f
Eric Patrick <erstpa@gmail.com>
2018-07-01 13:37:15 -0400

    Add project files.

---
+++ Einstein Project/Main Form.vb
@@ -0,0 +1,19 @@
+﻿Public Class frmMain
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Close the application. 
+
+        Me.Close()
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        ' Show Einstein's equation.
+
+        picEquation.Visible = True
+    End Sub
+
+    Private Sub btnHide_Click(sender As Object, e As EventArgs) Handles btnHide.Click
+        ' Hide Einstein's equation.
+
+        picEquation.Visible = False
+    End Sub
+End Class
